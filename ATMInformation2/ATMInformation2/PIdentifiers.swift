bd9933c43ba80d7f5649d63526bd78645b2e8764
SuryaKothapalli <surya@touchmeme.com>
2019-04-21 09:33:11 +0530

    PIdentifier.swift is added to capture all the identifiers used in the project LoginViewController is created and Login UI is updated

---
+++ ATMInformation2/ATMInformation2/PIdentifiers.swift
@@ -0,0 +1,15 @@
+//
+//  PIdentifiers.swift
+//  ATMInformation2
+//
+//  Created by Apple on 21/04/19.
+//  Copyright © 2019 Surya. All rights reserved.
+//
+
+import Foundation
+enum SegueIdentifiers : String{
+    case addAtmSegue = "addAtmSegue"
+    case atmListSegue = "atmListSegue"
+    case adminFlowSegue = "adminFlowSegue"
+    case userFlowSegue = "userFlowSegue"
+}
